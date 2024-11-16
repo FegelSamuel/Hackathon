3b44d476964497e20e8b945b68388edd1a50090e
fegelsamuel <fegelsamuel@gmail.com>
2024-11-16 15:23:41 -0600

    npm run dev works

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      AUTH0_CLIENT_ID: process.env.NUXT_AUTH0_CLIENT_ID || '',
+      AUTH0_DOMAIN: process.env.NUXT_AUTH0_DOMAIN || '',
+      AUTH0_CALLBACK_URL: process.env.NUXT_AUTH0_CALLBACK_URL || 'http://localhost:3000/api/callback',
+      },
+    AUTH0_SECRET: process.env.NUXT_AUTH0_CLIENT_SECRET || '',
+    ISSUER: process.env.NUXT_AUTH0_ISSUER || '',
+    BASEURL: process.env.BASEURL || 'http://localhost:3000/',
+    },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+   router: {
+    middleware: ['auth']
+  } 
 })
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import type { User } from '@/types.d.ts'
+  import { computed, ref } from 'vue'
+  const runtimeConfig = useRuntimeConfig()
+  console.log(runtimeConfig)
+
+const user = useCookie<User>('user');
+const isLoggedIn = computed(() => (user.value?.cuid != null)) // linter is mad here, don't worry about it
+const email = ref(user.value?.email)
+const role = ref(user.value?.user_role)
+</script>
+
+<template>
+<div>
+  <button v-if="isLoggedIn"></button>
+    <a href="/api/logout"> Logout </a>
+  <button v-if="!isLoggedIn"></button>
+    <a href="/api/login"> Login </a>
+</div>
+</template>
